057ce3c889b69561ea11c29a72abd69a753e5cf4
yogesh.upadhyay <yogesh.upadhyay@think360.ai>
2023-07-02 20:22:10 +0530

    Update settings and swagger configurations

--- null_jobs_backend/settings.py
+++ null_jobs_backend/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -20,10 +20,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-lvp7&6jby#o_p%)&4q^@xlbr-qf)v+zhr2#^limqt==g$y&ltr'
+SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'UPDATE-ME-DURING-RUNTIME')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get('DJANGO_DEBUG', '') == 'True'
 
 ALLOWED_HOSTS = []
 
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'rest_framework'
+    'rest_framework',
+    'drf_yasg'
 ]
 
 MIDDLEWARE = [
--- null_jobs_backend/urls.py
+++ null_jobs_backend/urls.py
@@ -16,9 +16,31 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from apps import accounts
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="null jobs backend",
+        default_version='v1',
+        description="API documentation for null jobs portal",
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('' ,  include('apps.accounts.urls', namespace="accounts" ))
+    path('' ,  include('apps.accounts.urls', namespace="accounts")),
+    path(
+        'api/docs/', 
+        schema_view.with_ui('swagger', cache_timeout=0), 
+        name='schema-swagger-ui'
+    ),
+    path(
+        'api/redoc/', 
+        schema_view.with_ui('redoc', cache_timeout=0), 
+        name='schema-redoc'
+    )
 ]
