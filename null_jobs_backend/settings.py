7c11f4b3ee12cfd5a0b079b624e392d2b930155c
Anubhav Shukla <97546428+mr-robot369@users.noreply.github.com>
2023-09-10 12:09:12 +0530

    Update settings.py
    Fixed :
    Login Flow
    
    Added : 
    Email Configuration, Google Auth, Cors headed

--- null_jobs_backend/settings.py
+++ null_jobs_backend/settings.py
@@ -37,7 +37,12 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "django.contrib.sites",  # needed for oauth very importatnt
+    "dj_rest_auth", # use for google authentication
+    "corsheaders",
     "rest_framework",
+    "rest_framework.authtoken",
+    "rest_framework_simplejwt",
     "drf_yasg",
     "apps.accounts",
     "apps.jobs",
@@ -46,6 +51,7 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
+    "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
@@ -88,7 +94,6 @@ DATABASES = {
     }
 }
 
-
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
 
@@ -107,6 +112,58 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
+# User Model
+AUTH_USER_MODEL = 'accounts.User'
+
+# google auth configuration
+SOCIALACCOUNT_PROVIDERS = {
+   'google': {
+       'APP':{
+           "client_id":os.environ.get("GOOGLE_OAUTH_CLIENT_ID"),
+           "secret":os.environ.get("GOOGLE_OAUTH_SECRET"),
+       },
+      'SCOPE': [
+         'profile',
+         'email',
+      ],
+      'AUTH_PARAMS': {
+         'access_type': 'online',
+      }
+   }
+}
+
+# google auth settings
+ACCOUNT_AUTHENTICATION_METHOD="email"
+ACCOUNT_EMAIL_REQUIRED=True
+ACCOUNT_UNIQUE_EMAIL=True
+ACCOUNT_USERNAME_REQUIRED=False
+ACCOUNT_USER_MODEL_USERNAME_FIELD=None
+GOOGLE_REDIRECT_URI="http://localhost:8000/google/login/callback/"
+
+# dj-rest-auth setting
+JWT_AUTH_SECURE = True
+REST_USE_JWT=True
+JWT_AUTH_COOKIE= 'access_token'
+JWT_AUTH_REFRESH_COOKIE= 'refresh-token'
+
+USE_JWT=True
+
+
+# Email Configuration
+EMAIL_BACKEND="django.core.mail.backends.smtp.EmailBackend"
+EMAIL_HOST='smtp.gmail.com'
+EMAIL_PORT=587
+EMAIL_HOST_USER= os.environ.get('EMAIL_USER')
+EMAIL_HOST_PASSWORD= os.environ.get('EMAIL_PASS')
+EMAIL_USE_TLS=True
+
+# JWT Configuration
+REST_FRAMEWORK = {
+    'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework_simplejwt.authentication.JWTAuthentication',
+        'dj_rest_auth.jwt_auth.JWTCookieAuthentication'   # dj-rest-auth for jwt
+    ),
+}
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
@@ -169,17 +226,9 @@ CORS_ALLOWED_ORIGINS = [
 
 
 AUTHENTICATION_BACKENDS = [
-    # ...
     # Needed to login by username in Django admin, regardless of `allauth`
-    "django.contrib.auth.backends.ModelBackend",
-    # `allauth` specific authentication methods, such as login by e-mail
-    "allauth.account.auth_backends.AuthenticationBackend",
-    # ...
+    'django.contrib.auth.backends.ModelBackend',
 ]
 
 # GOOGLE AUTH
 SITE_ID = 2
-
-# LOGIN_REDIRECT_URL = '/'
-
-# ACCOUNT_ADAPTER = 'accountapp.adapters.CustomAccountAdapter'
