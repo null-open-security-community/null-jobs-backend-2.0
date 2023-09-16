380e29f142bad8c57ebbadaa6dd8f8a1c46a440a
yogesh.upadhyay <yogesh.upadhyay@think360.ai>
2023-09-16 23:14:50 +0530

    Fixes OTP issue

--- apps/accounts/utils.py
+++ apps/accounts/utils.py
@@ -4,7 +4,6 @@ import pyotp
 from django.core.mail import EmailMessage
 
 
-#Todo: email_template has to be added
 class Util:
     @staticmethod
     def send_email(data):
@@ -16,14 +15,28 @@ class Util:
         )
         email.send()
 
+
 class OTP:
+    # generate secret for a new user
     @staticmethod
-    def generate_otp():
+    def generate_secret_with_otp():
         base32secret3232 = pyotp.random_base32()
-        otp = pyotp.TOTP(base32secret3232, interval=300, digits=6)
+        otp = pyotp.TOTP(
+            base32secret3232, interval=300, digits=6
+        )  # otp valid for 5 minutes (300sec)
         time_otp = otp.now()
         return time_otp, base32secret3232
 
+    # generate otp for a user
+    @staticmethod
+    def generate_otp(user):
+        otp = pyotp.TOTP(
+            user.otp_secret, interval=300, digits=6
+        )  # otp valid for 5 minutes (300sec)
+        time_otp = otp.now()
+        return time_otp
+
+    # verift otp
     @staticmethod
     def verify_otp(user, otp):
         return pyotp.TOTP(user.otp_secret, interval=300, digits=6).verify(otp)
