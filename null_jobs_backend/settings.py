ff8d679ccb14adc55fe1dc1761f111e4fef34142
Himanshu Sharma <sharmahimanshu1911@gmail.com>
2024-01-27 19:15:37 +0530

    Update settings.py

--- null_jobs_backend/settings.py
+++ null_jobs_backend/settings.py
@@ -222,6 +222,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000",  # 3000 port is here for react app
     "http://localhost:5173",
     "http://127.0.0.1:5173",
+    "http://localhost:5173/",
 ]
 
 AUTHENTICATION_BACKENDS = [
