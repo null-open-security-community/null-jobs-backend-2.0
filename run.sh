9ff3df9f4b46cf6362686d24ac4334c69d405dd8
Yogesh Upadhyay <53992168+YogeshUpdhyay@users.noreply.github.com>
2024-02-17 23:55:33 +0530

    Staging (#3)

--- run.sh
+++ run.sh
@@ -4,4 +4,3 @@ python manage.py makemigrations
 python manage.py makemigrations apps.jobs
 python manage.py makemigrations apps.accounts
 python manage.py migrate
-python manage.py runserver 0.0.0.0:8000
