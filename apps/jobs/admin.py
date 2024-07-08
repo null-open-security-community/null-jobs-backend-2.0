f81d07e631f83721c5aa5ee5324941896cd2b6be
HimanshuSharma-Angel <Himanshu.1Sharma@angelbroking.com>
2024-07-08 19:04:21 +0530

    Initial commit for django admin.

--- apps/jobs/admin.py
+++ apps/jobs/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Job, ContactMessage, Company
 # Register your models here.
+
+admin.site.register(Job)
+admin.site.register(ContactMessage)
+admin.site.register(Company)
